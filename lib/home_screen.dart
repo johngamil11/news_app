9c054047faab64675c7223b5301770d8a5a25dc5
Kimo Store <johngamil52@gmail.com>
2024-08-12 23:21:19 +0200

    started the app.

---
+++ lib/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String RouteName = 'home_screen' ;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/home_screen.dart';
+
+void main (){
+  runApp(MyApp ());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.RouteName ,
+      routes:{ HomeScreen.RouteName : (context) => HomeScreen(),
+      }
+    );
+  }
+
+}
